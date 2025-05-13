c1e25eb186f67819c1f46d320c2005ee07b9a246
Anko <122093080+bukananko@users.noreply.github.com>
2025-05-13 11:05:16 +0700

    fix parallax effect

--- src/components/ParallaxSection.tsx
+++ src/components/ParallaxSection.tsx
@@ -4,7 +4,7 @@ import { Parallax } from 'react-parallax'
 
 const ParallaxSection = () => {
   return (
-    <Parallax bgImage="/bgresto.png">
+    <Parallax bgImage="/bgresto.png" strength={-200} bgClassName="object-cover">
       <div className="container flex justify-end items-end pb-20 pt-80">
         <div className="text-5xl text-white">
           ALWAYS <br />
